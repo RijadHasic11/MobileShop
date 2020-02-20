9f4ceb1903a87c504b63491b2d4f99508f43f103
Emin404 <emin.adilovic@edu.fit.ba>
2020-02-21 00:19:17 +0100

    frmadmin
    frmadmin frmadmin frmadmin

---
+++ MobileShop.Model/Migrations/20200220212833_2022020.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MobileShop.Model.Migrations
+{
+    public partial class _2022020 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
